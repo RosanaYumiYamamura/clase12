8be49e8256b3875a960d4a69eaa327a7d55a20a3
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-04-20 20:04:53 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+
+import * as rls from "readline-sync";
+let arr1: number[] = new Array(6);
+let arr2: number[] = new Array(6);
+let arr3: number[] = new Array(6);
+
+
+for (let i: number = 0; i < arr1.length; i++) {
+  console.clear();
+  arr1[i] = rls.questionInt(`Arr#1 -> number in position ${i + 1}: `);
+  arr2[i] = rls.questionInt(`Arr#2 -> number in position ${i + 1}: `);
+}
+
+for (let i: number = 0; i < arr3.length; i++) {
+    arr3[i] = arr1[i]+ arr2[i];
+}
+console.log(arr3);
